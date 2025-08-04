445edbcd3048fdf380cf8413adf8fe4b0fe65844
luka-dv <luka.guva@gmail.com>
2025-08-04 10:54:13 +0200

    add startREPL func

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,4 @@
+import { createInterface } from "node:readline";
 
 export function cleanInput(input: string): string[] {
     const splitLowercaseTrim = input
@@ -8,4 +9,23 @@ export function cleanInput(input: string): string[] {
     return splitLowercaseTrim;
 }
 
+export function  startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex >"
+    });
+
+    rl.prompt();
+
+    rl.on('line', (line) => {
+        const text = cleanInput(line);
+        if(text.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${text[0]}`);
+        rl.prompt()
+    });
+}
  
