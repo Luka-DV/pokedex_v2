be2cc9098c7a43f19096f9d85950ae5e5f19f963
luka-dv <luka.guva@gmail.com>
2025-08-09 10:18:13 +0200

    add fetchLocation logic

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -21,20 +21,74 @@ export class PokeAPI {
         }
     }
 
-    /* async fetchLocation(locationName: string): Promise<Location> {
+    async fetchLocation(locationName: string): Promise<Location> {
+      
+      const resURL = `${PokeAPI.baseURL}/location/${locationName}/`
+      try {
+        const res = await fetch(resURL);
+        if(!res.ok) {
+            throw new Error(`Response status: ${res.status}`);
+        }
+        
+        const data = await res.json();
+        return data;
 
-    } */
+      } catch (err) {
+          throw err;        
+      }
+
+    }
 }
 
 export type ShallowLocations = {
   count: number
   next: string
   previous: any
-  results: Location[]
+  results: Result[]
 
 }
+export type Result = {
+  name: string
+  url: string
+}
 
-export type Location = {
+
+
+export interface Location {
+  areas: Area[]
+  game_indices: Index[]
+  id: number
+  name: string
+  names: Name[]
+  region: Region
+}
+
+export interface Area {
+  name: string
+  url: string
+}
+
+export interface Index {
+  game_index: number
+  generation: Generation
+}
+
+export interface Generation {
+  name: string
+  url: string
+}
+
+export interface Name {
+  language: Language
+  name: string
+}
+
+export interface Language {
+  name: string
+  url: string
+}
+
+export interface Region {
   name: string
   url: string
 }
