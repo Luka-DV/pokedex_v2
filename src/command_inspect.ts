83bffe04145959c32856d66c5790592d63ec025c
luka-dv <luka.guva@gmail.com>
2025-08-17 19:54:14 +0200

    implement inspect command

---
+++ src/command_inspect.ts
@@ -0,0 +1,20 @@
+
+import { State } from "./state.js";
+
+export async function commandInspect(state: State, pokemon: string) {
+
+    const pokemonData = state.pokedex?.[pokemon];
+
+    if(!pokemonData) {
+        console.log(`You have not caught ${pokemon} yet`);
+        return;
+    }
+
+    console.log(`Name: ${pokemonData.name}`);
+    console.log(`Height: ${pokemonData.height}`);
+    console.log(`Weight: ${pokemonData.weight}`); 
+    console.log(`Stats:`);
+    pokemonData.stats.forEach(stat => console.log(` -${stat.stat.name}: ${stat.base_stat}`));
+    console.log("Types:");
+    pokemonData.types.forEach(type => console.log(` -${type.type.name}`));
+}
--- src/commands_all.ts
+++ src/commands_all.ts
@@ -6,6 +6,7 @@ import { commandMap } from "./command_map.js"
 import { commandMapB } from "./command_mapb.js"
 import { commandExplore } from "./command_explore.js"
 import { commandCatch } from "./command_catch.js"
+import { commandInspect } from "./command_inspect.js"
 
 export function getCommands(): Record<string, CLICommand> {
     return {
@@ -38,6 +39,11 @@ export function getCommands(): Record<string, CLICommand> {
             name: "catch",
             description: "Try to catch a pokemon",
             callback: commandCatch
+        },
+        inspect: {
+            name: "inspect",
+            description: "Inspect a pokemon you have caught",
+            callback: commandInspect
         }
     }
 }
